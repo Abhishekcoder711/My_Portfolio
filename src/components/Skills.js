ec9f157b0b1e2a8f9758af6a034b1106c984a02b
Abhishekcoder711 <abhisehkind711@gmail.com>
2026-02-23 00:32:05 +0530

    Update

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { FaReact, FaNodeJs, FaHtml5, FaCss3Alt, FaJsSquare, FaPython, FaGithub, FaGit } from "react-icons/fa";
-import { SiCplusplus, SiMysql, SiPostman, SiTableau, SiMicrosftPowerbi, SiMicrosoftexcel, SiNetlify, SiMongodb, SiMicrosoftazure, SiRender } from "react-icons/si";
+import { SiCplusplus, SiMysql, SiPostman, SiTableau, SiMicrosoftexcel, SiNetlify, SiMongodb, SiMicrosoftazure, SiRender } from "react-icons/si";
 import { SiExpress } from "react-icons/si";
+import { SiPowerbi } from "react-icons/si";
 import "./Skills.css";
 
 function Skills() {
   const skills = [
     { icon: <FaPython className="skill-icon python" />, name: "Python" },
     { icon: <SiTableau className="skill-icon tableau" />, name: "Tableau" },
-    { icon: <SiMicrosftPowerbi className="skill-icon powerbi" />, name: "Power BI" },
+    { icon: <SiPowerbi className="skill-icon powerbi" />, name: "Power BI" },
     { icon: <SiMysql className="skill-icon mysql" />, name: "MySQL" },
     { icon: <SiMicrosoftexcel className="skill-icon excel" />, name: "Excel" },
     // { icon: <SiCplusplus className="skill-icon cpp" />, name: "C++" },
